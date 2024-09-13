31f7ff36d563d988b3d9b9edbad202b4d777333d
kauncoder <snehsocial@protonmail.com>
2024-09-13 18:43:34 +0200

    testing with raw bytes

--- src/blake_hash.rs
+++ src/blake_hash.rs
@@ -5,11 +5,11 @@ use std::{
     path::Path,
 };
 
-pub const OFFSET_ONE: [u8; 4] = 1u32.to_le_bytes();
+pub const _OFFSET_ONE: [u8; 4] = 1u32.to_le_bytes();
 //pub const TEST_DIR: &str = "./testfiles";
 
 pub fn run_blake3() {
-    let file_list: Vec<String> = get_file_list("testfiles_bad");
+    let file_list: Vec<String> = get_file_list("testfiles_good");
     let (file_hash_list, file_content_list) = get_file_hashes(file_list);
     let file_hashes = file_hash_list
         .iter()
@@ -37,22 +37,37 @@ pub fn get_file_list(upload_dir: &str) -> Vec<String> {
 pub fn get_file_hashes(file_list: Vec<String>) -> (Vec<Hash>, Vec<Vec<u8>>) {
     //read files and return vec of file hashes
     let mut file_hash_list: Vec<Hash> = Vec::new();
-    let mut file_content_list: Vec<Vec<u8>> = Vec::new();
+    let mut _file_content_list: Vec<Vec<u8>> = Vec::new();
     for file in file_list.clone() {
         let mut file = fs::File::open(Path::new(&file)).unwrap();
-        let mut content = String::new();
-        file.read_to_string(&mut content).unwrap();
-        //remove windows carraige return from file \r
-        content = content.replace("\r", "");
-        let file_content = content.as_bytes().to_vec();
-        file_content_list.push(file_content.clone());
-        let mut hash = blake3::Hasher::new();
-        hash.update(&OFFSET_ONE);
-        hash.update(&file_content);
-        let hash = hash.finalize();
+        // let mut content = String::new();
+        // file.read_to_string(&mut content).unwrap();
+        // //remove windows carraige return from file \r
+        // content = content.replace("\r", "");
+        // let file_content = content.as_bytes().to_vec();
+        // file_content_list.push(file_content.clone());
+        // let mut hash = blake3::Hasher::new();
+        // hash.update(&OFFSET_ONE);
+        // hash.update(&file_content);
+        // let hash = hash.finalize();
+        // file_hash_list.push(hash);
+        // Buffer to read chunks of file
+        let mut buffer = [0u8; 4096];
+        let mut hasher = blake3::Hasher::new();
+
+        // Read file in chunks
+        loop {
+            let bytes_read = file.read(&mut buffer).unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+            // Feed the read bytes into the hash function
+            hasher.update(&buffer[..bytes_read]);
+        }
+        let hash = hasher.finalize();
         file_hash_list.push(hash);
     }
-    (file_hash_list, file_content_list)
+    (file_hash_list, _file_content_list)
 }
 
 #[cfg(test)] // This annotation ensures that the following code is only compiled when testing
@@ -64,12 +79,12 @@ mod tests {
         //load then file contents
         let known_value: Vec<Vec<u8>> = vec![
             vec![
-                193, 147, 145, 5, 39, 26, 61, 181, 134, 92, 230, 178, 35, 170, 228, 50, 243, 230,
-                64, 67, 68, 239, 45, 57, 144, 224, 228, 243, 242, 180, 45, 51,
+                76, 207, 203, 212, 211, 104, 6, 239, 192, 57, 64, 219, 157, 67, 159, 203, 159, 178,
+                250, 169, 181, 207, 174, 107, 150, 188, 58, 119, 147, 41, 180, 121,
             ],
             vec![
-                64, 2, 61, 59, 61, 22, 48, 75, 149, 110, 221, 51, 232, 2, 29, 237, 201, 217, 81,
-                200, 73, 11, 159, 208, 189, 167, 148, 161, 13, 30, 60, 68,
+                182, 126, 198, 177, 40, 24, 145, 166, 36, 85, 0, 104, 226, 13, 9, 255, 115, 78,
+                146, 59, 182, 117, 134, 108, 80, 177, 39, 169, 188, 130, 58, 254,
             ],
         ];
         let file_list: Vec<String> = get_file_list("testfiles_good");
@@ -84,7 +99,7 @@ mod tests {
     #[test]
     fn test_run_bad() {
         //load then file contents
-        let known_file_content: Vec<Vec<u8>> = vec![
+        let _known_file_content: Vec<Vec<u8>> = vec![
             vec![
                 116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 105, 109, 112, 108, 101, 32,
                 116, 120, 116, 32, 102, 105, 108, 101, 32, 116, 111, 32, 99, 104, 101, 99, 107, 32,
@@ -100,22 +115,32 @@ mod tests {
         ];
         let known_value: Vec<Vec<u8>> = vec![
             vec![
-                228, 101, 232, 48, 200, 194, 4, 2, 194, 119, 62, 73, 137, 5, 214, 56, 179, 176,
-                223, 102, 137, 218, 201, 138, 51, 1, 49, 214, 207, 70, 84, 40,
+                127, 12, 85, 134, 164, 42, 41, 124, 45, 137, 239, 42, 97, 32, 203, 121, 119, 19,
+                185, 65, 125, 137, 10, 88, 195, 111, 80, 5, 71, 76, 63, 153,
             ],
             vec![
-                8, 210, 243, 221, 251, 130, 213, 48, 131, 91, 51, 207, 141, 104, 211, 125, 97, 74,
-                253, 57, 125, 181, 32, 17, 110, 75, 171, 130, 22, 42, 27, 235,
+                66, 25, 178, 204, 202, 140, 244, 92, 254, 240, 121, 154, 226, 221, 118, 66, 223,
+                59, 92, 210, 3, 27, 156, 225, 48, 86, 123, 162, 27, 229, 183, 90,
             ],
         ];
+        // let known_value: Vec<Vec<u8>> = vec![
+        //     vec![
+        //         228, 101, 232, 48, 200, 194, 4, 2, 194, 119, 62, 73, 137, 5, 214, 56, 179, 176,
+        //         223, 102, 137, 218, 201, 138, 51, 1, 49, 214, 207, 70, 84, 40,
+        //     ],
+        //     vec![
+        //         8, 210, 243, 221, 251, 130, 213, 48, 131, 91, 51, 207, 141, 104, 211, 125, 97, 74,
+        //         253, 57, 125, 181, 32, 17, 110, 75, 171, 130, 22, 42, 27, 235,
+        //     ],
+        // ];
 
         let file_list: Vec<String> = get_file_list("testfiles_bad");
-        let (computed_hashes, computed_contents) = get_file_hashes(file_list);
+        let (computed_hashes, _computed_contents) = get_file_hashes(file_list);
         let computed_value = computed_hashes
             .iter()
             .map(|h| h.as_bytes().to_vec())
             .collect::<Vec<_>>();
-        assert_eq!(known_file_content, computed_contents);
+        //  assert_eq!(known_file_content, computed_contents);
         assert_eq!(known_value, computed_value);
         //assert_eq!(known_value, computed_value);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod blake_hash;
-mod sha3_hash;
+//mod sha3_hash;
 fn main() {
     blake_hash::run_blake3();
-    sha3_hash::run_sha3();
+    // sha3_hash::run_sha3();
 }
