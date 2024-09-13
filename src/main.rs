ed30e230db713a37458a819a6d0966dd795c9254
kauncoder <snehsocial@protonmail.com>
2024-09-13 13:14:22 +0200

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -27,24 +27,24 @@ fn run_blake3(mut hasher: blake3::Hasher, contents: Vec<u8>) -> Hash {
 #[cfg(test)] // This annotation ensures that the following code is only compiled when testing
 mod tests {
     #[test]
-    fn test_run_blaake3(){
+    fn test_run_blaake3() {
         use std::{
             fs::File,
             io::{BufReader, Read},
         };
-    
+
         use crate::{run_blake3, TEST_FILE_PATH};
-         //load then file contents
-    let known_value = "c1939105271a3db5865ce6b223aae432f3e6404344ef2d3990e0e4f3f2b42d33".to_string();
-    let file = File::open(TEST_FILE_PATH).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut contents = Vec::new();
-    reader.read_to_end(&mut contents).unwrap();
+        //load then file contents
+        let known_value =
+            "c1939105271a3db5865ce6b223aae432f3e6404344ef2d3990e0e4f3f2b42d33".to_string();
+        let file = File::open(TEST_FILE_PATH).unwrap();
+        let mut reader = BufReader::new(file);
+        let mut contents = Vec::new();
+        reader.read_to_end(&mut contents).unwrap();
 
-    let hasher_blake3 = blake3::Hasher::new();
-    let hash = run_blake3(hasher_blake3, contents);
-    let computed_value = format!("{}", hash.to_hex());
-    assert_eq!(known_value,computed_value);
-
+        let hasher_blake3 = blake3::Hasher::new();
+        let hash = run_blake3(hasher_blake3, contents);
+        let computed_value = format!("{}", hash.to_hex());
+        assert_eq!(known_value, computed_value);
     }
 }
