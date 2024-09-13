bd84383662ac8380c16bb7317cf6ed7b3275ca46
kauncoder <snehsocial@protonmail.com>
2024-09-13 14:15:32 +0200

    modify tests

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
 use blake3::Hash;
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{BufReader, Read},
+    path::Path,
 };
 
 pub const OFFSET_ONE: [u8; 4] = 1u32.to_le_bytes();
-pub const TEST_FILE_PATH: &str = "./testfiles/test.txt";
+pub const TEST_DIR: &str = "./testfiles";
 
 fn main() {
-    //load then file contents
-    let file = File::open(TEST_FILE_PATH).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut contents = Vec::new();
-    reader.read_to_end(&mut contents).unwrap();
-
-    let hasher_blake3 = blake3::Hasher::new();
-    let _hash = run_blake3(hasher_blake3, contents);
-    //println!("hash value is : {:?}", hash.as_bytes().to_vec());
+    let file_list: Vec<String> = get_file_list(TEST_DIR);
+    let file_hashes = get_file_hashes(file_list)
+        .iter()
+        .map(|h| h.as_bytes().to_vec())
+        .collect::<Vec<_>>();
+    println!("file hashes : {:?}", file_hashes);
 }
 
-fn run_blake3(mut hasher: blake3::Hasher, contents: Vec<u8>) -> Hash {
-    hasher.update(&OFFSET_ONE);
-    hasher.update(&contents);
-    hasher.finalize()
+fn get_file_list(upload_dir: &str) -> Vec<String> {
+    let mut file_list: Vec<String> = Vec::new(); //replace with more concrete type
+    let dir_path = format!("./{}", upload_dir);
+    let dir = Path::new(&dir_path);
+    let entries = fs::read_dir(dir).unwrap();
+    for entry in entries {
+        let path = entry.unwrap().path();
+        file_list.push(path.display().to_string())
+    }
+    file_list.sort();
+    file_list
 }
+
+fn get_file_hashes(file_list: Vec<String>) -> Vec<Hash> {
+    //read files and return vec of file hashes
+    let mut file_hash_list: Vec<Hash> = Vec::new();
+    for file in file_list.clone() {
+        let file_content = std::fs::read(file.clone()).unwrap();
+        let mut hash = blake3::Hasher::new();
+        hash.update(&OFFSET_ONE);
+        hash.update(&file_content);
+        let hash = hash.finalize();
+        file_hash_list.push(hash);
+    }
+    file_hash_list
+}
+
 #[cfg(test)] // This annotation ensures that the following code is only compiled when testing
 mod tests {
+    use crate::{get_file_hashes, get_file_list, TEST_DIR};
+
     #[test]
     fn test_run_blaake3() {
         use std::{
@@ -33,20 +55,23 @@ mod tests {
             io::{BufReader, Read},
         };
 
-        use crate::{run_blake3, TEST_FILE_PATH};
         //load then file contents
-        let known_value: Vec<u8> = vec![
-            193, 147, 145, 5, 39, 26, 61, 181, 134, 92, 230, 178, 35, 170, 228, 50, 243, 230, 64,
-            67, 68, 239, 45, 57, 144, 224, 228, 243, 242, 180, 45, 51,
+        let known_value: Vec<Vec<u8>> = vec![
+            vec![
+                193, 147, 145, 5, 39, 26, 61, 181, 134, 92, 230, 178, 35, 170, 228, 50, 243, 230,
+                64, 67, 68, 239, 45, 57, 144, 224, 228, 243, 242, 180, 45, 51,
+            ],
+            vec![
+                64, 2, 61, 59, 61, 22, 48, 75, 149, 110, 221, 51, 232, 2, 29, 237, 201, 217, 81,
+                200, 73, 11, 159, 208, 189, 167, 148, 161, 13, 30, 60, 68,
+            ],
         ];
-        let file = File::open(TEST_FILE_PATH).unwrap();
-        let mut reader = BufReader::new(file);
-        let mut contents = Vec::new();
-        reader.read_to_end(&mut contents).unwrap();
 
-        let hasher_blake3 = blake3::Hasher::new();
-        let hash = run_blake3(hasher_blake3, contents);
-        let computed_value = hash.as_bytes().to_vec();
+        let file_list: Vec<String> = get_file_list(TEST_DIR);
+        let computed_value = get_file_hashes(file_list)
+            .iter()
+            .map(|h| h.as_bytes().to_vec())
+            .collect::<Vec<_>>();
         assert_eq!(known_value, computed_value);
     }
 }
